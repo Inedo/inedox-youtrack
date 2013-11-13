675068ee3be04a3ea5b352e0d4ca2f806daa1c92
kharnagy <kharnagy@inedo.com>
2013-11-13 13:02:45 -0500

    Initial commit

---
+++ YouTrackIssueTrackingProvider.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Linq;
+using Inedo.BuildMaster;
+using Inedo.BuildMaster.Extensibility.Providers;
+using Inedo.BuildMaster.Extensibility.Providers.IssueTracking;
+using Inedo.BuildMaster.Web;
+
+namespace Inedo.BuildMasterExtensions.YouTrack
+{
+    [ProviderProperties(
+        "YouTrack",
+        "Issue tracking provider for JetBrains YouTrack.")]
+    [CustomEditor(typeof(YouTrackIssueTrackingProviderEditor))]
+    public sealed class YouTrackIssueTrackingProvider : IssueTrackingProviderBase, ICategoryFilterable, IUpdatingProvider
+    {
+        private Lazy<YouTrackSession> session;
+
+        public YouTrackIssueTrackingProvider()
+        {
+            this.MaxIssues = 50;
+            this.ReleaseField = "Fix versions";
+            this.session = new Lazy<YouTrackSession>(() => new YouTrackSession(this.BaseUrl, this.UserName, this.Password, this.ReleaseField));
+        }
+
+        [Persistent]
+        public string BaseUrl { get; set; }
+        [Persistent]
+        public string UserName { get; set; }
+        [Persistent]
+        public string Password { get; set; }
+        [Persistent]
+        public string ReleaseField { get; set; }
+        [Persistent]
+        public int MaxIssues { get; set; }
+        [Persistent]
+        public string[] CategoryIdFilter { get; set; }
+        public string[] CategoryTypeNames
+        {
+            get { return new[] { "Project" }; }
+        }
+        bool IUpdatingProvider.CanAppendIssueDescriptions
+        {
+            get { return true; }
+        }
+        bool IUpdatingProvider.CanChangeIssueStatuses
+        {
+            get { return true; }
+        }
+        bool IUpdatingProvider.CanCloseIssues
+        {
+            get { return false; }
+        }
+
+        public override string GetIssueUrl(IssueTrackerIssue issue)
+        {
+            if (issue == null)
+                throw new ArgumentNullException("issue");
+
+            var url = this.BaseUrl;
+            if (!url.EndsWith("/"))
+                url += "/";
+
+            return url + "issue/" + Uri.EscapeUriString(issue.IssueId);
+        }
+        public IssueTrackerCategory[] GetCategories()
+        {
+            return this.session.Value.GetProjects().ToArray();
+        }
+        public override IssueTrackerIssue[] GetIssues(string releaseNumber)
+        {
+            if (string.IsNullOrEmpty(releaseNumber))
+                throw new ArgumentNullException("releaseNumber");
+
+            return this.session.Value.GetIssues(this.CategoryIdFilter[0], releaseNumber, this.MaxIssues).ToArray();
+        }
+        public override bool IsIssueClosed(IssueTrackerIssue issue)
+        {
+            if (issue == null)
+                throw new ArgumentNullException("issue");
+
+            return ((YouTrackIssue)issue).IsResolved;
+        }
+        public override bool IsAvailable()
+        {
+            return true;
+        }
+        public override string ToString()
+        {
+            return "Issue tracking provider for JetBrains YouTrack.";
+        }
+        public override void ValidateConnection()
+        {
+            if (!string.IsNullOrEmpty(this.UserName))
+                this.session.Value.Connect();
+            else
+                this.session.Value.GetProjects();
+        }
+        public void AppendIssueDescription(string issueId, string textToAppend)
+        {
+            if (string.IsNullOrEmpty(issueId))
+                throw new ArgumentNullException("issueId");
+            if (string.IsNullOrWhiteSpace(textToAppend))
+                return;
+
+            this.session.Value.ApplyCommandToIssue(issueId, "comment", textToAppend);
+        }
+        public void ChangeIssueStatus(string issueId, string newStatus)
+        {
+            if (string.IsNullOrEmpty(issueId))
+                throw new ArgumentNullException("issueId");
+            if (string.IsNullOrEmpty(newStatus))
+                throw new ArgumentNullException("newStatus");
+
+            this.session.Value.ApplyCommandToIssue(issueId, "state " + newStatus);
+        }
+        void IUpdatingProvider.CloseIssue(string issueId)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ YouTrackIssueTrackingProviderEditor.cs
@@ -0,0 +1,112 @@
+﻿using System.Web.UI.WebControls;
+using Inedo.BuildMaster.Extensibility.Providers;
+using Inedo.BuildMaster.Web.Controls;
+using Inedo.BuildMaster.Web.Controls.Extensions;
+using Inedo.Web.Controls;
+
+namespace Inedo.BuildMasterExtensions.YouTrack
+{
+    internal sealed class YouTrackIssueTrackingProviderEditor : ProviderEditorBase
+    {
+        private ValidatingTextBox txtBaseUrl;
+        private ValidatingTextBox txtUserName;
+        private PasswordTextBox txtPassword;
+        private ValidatingTextBox txtReleaseField;
+        private ValidatingTextBox txtMaxIssues;
+
+        public override bool DisplayLogCommandLineArgumentsCheckBox
+        {
+            get { return false; }
+        }
+
+        public override void BindToForm(ProviderBase extension)
+        {
+            this.EnsureChildControls();
+
+            var provider = (YouTrackIssueTrackingProvider)extension;
+            this.txtBaseUrl.Text = provider.BaseUrl;
+            this.txtUserName.Text = provider.UserName;
+            this.txtPassword.Text = provider.Password;
+            this.txtReleaseField.Text = provider.ReleaseField;
+            this.txtMaxIssues.Text = provider.MaxIssues.ToString();
+        }
+        public override ProviderBase CreateFromForm()
+        {
+            this.EnsureChildControls();
+
+            return new YouTrackIssueTrackingProvider
+            {
+                BaseUrl = this.txtBaseUrl.Text,
+                UserName = this.txtUserName.Text,
+                Password = this.txtPassword.Text,
+                ReleaseField = this.txtReleaseField.Text,
+                MaxIssues = int.Parse(this.txtMaxIssues.Text)
+            };
+        }
+
+        protected override void CreateChildControls()
+        {
+            this.txtBaseUrl = new ValidatingTextBox
+            {
+                Width = 300,
+                Required = true,
+                ValidationExpression = "^[hH][tT][tT][pP][sS]?://[^/]+.*",
+                DefaultText = "ex: http://test.myjetbrains.com/youtrack"
+            };
+
+            this.txtUserName = new ValidatingTextBox
+            {
+                Width = 300,
+                DefaultText = "anonymous"
+            };
+
+            this.txtPassword = new PasswordTextBox
+            {
+                Width = 250
+            };
+
+            this.txtReleaseField = new ValidatingTextBox
+            {
+                Width = 300,
+                Required = true,
+                Text = "Fix versions"
+            };
+
+            this.txtMaxIssues = new ValidatingTextBox
+            {
+                Width = 200,
+                Required = true,
+                Type = ValidationDataType.Integer,
+                Text = "50"
+            };
+
+            this.Controls.Add(
+                new FormFieldGroup(
+                    "YouTrack Address",
+                    "Provide the URL of your YouTrack instance.",
+                    false,
+                    new StandardFormField("Address:", this.txtBaseUrl)
+                ),
+                new FormFieldGroup(
+                    "Credentials",
+                    "Specify the user name and password to use to connect to YouTrack. You may leave these blank to connect anonymously if your YouTrack instance supports guest access.",
+                    false,
+                    new StandardFormField("User Name:", this.txtUserName),
+                    new StandardFormField("Password:", this.txtPassword)
+                ),
+                new FormFieldGroup(
+                    "Mapping",
+                    "Specify the name of the field to use in YouTrack which will contain the associated BuildMaster release number of an issue. By default, this is <i>Fix versions</i>.",
+                    false,
+                    new StandardFormField("Release Field:", this.txtReleaseField)
+                ),
+                new FormFieldGroup(
+                    "Limits",
+                    "Specify the maximum number of issues to request from the YouTrack instance in a request.",
+                    false,
+                    new StandardFormField("Maximum Issues:", this.txtMaxIssues)
+                )
+            );
+        }
+    }
+}
