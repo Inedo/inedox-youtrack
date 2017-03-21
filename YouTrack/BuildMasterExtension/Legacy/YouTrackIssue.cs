e3be087feebfb3603791cfca5d9e4a502aa64339
Ben Lubar <ben.lubar@gmail.com>
2017-03-21 13:35:22 -0500

    Convert to inedox layout (#3)

---
+++ YouTrack/BuildMasterExtension/Legacy/YouTrackIssue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Inedo.BuildMaster.Extensibility.Providers.IssueTracking;
+
+namespace Inedo.BuildMasterExtensions.YouTrack
+{
+    [Serializable]
+    internal sealed class YouTrackIssue : IssueTrackerIssue
+    {
+        public YouTrackIssue(string issueId, string issueStatus, string issueTitle, string issueDescription, string releaseNumber, bool isResolved)
+            : base(issueId, issueStatus, issueTitle, issueDescription, releaseNumber)
+        {
+            this.IsResolved = isResolved;
+        }
+
+        public bool IsResolved { get; private set; }
+    }
+}
