ada14948fb60783d31c79e15e4895580c18a0130
Greg D <gdivis@inedo.com>
2018-02-14 17:47:59 -0500

    updated for buildmaster 6.0

--- YouTrack/InedoExtension/Properties/AssemblyInfo.cs
+++ YouTrack/InedoExtension/Properties/AssemblyInfo.cs
@@ -1,3 +1,14 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
 
-[assembly: AssemblyProduct("Otter")]
+[assembly: AssemblyTitle("YouTrack")]
+[assembly: AssemblyDescription("Contains an issue tracking source for JetBrains YouTrack.")]
+
+[assembly: ComVisible(false)]
+[assembly: AssemblyCompany("Inedo, LLC")]
+[assembly: AssemblyCopyright("Copyright © 2008 - 2018")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0")]
+[assembly: CLSCompliant(false)]
+[assembly: AssemblyProduct("any")]
