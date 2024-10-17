3351bd664515aada4ec1c71344339d280b88b529
Greg Divis <gdivis@inedo.com>
2024-10-17 15:02:44 -0400

    update to sdk 3.1

--- YouTrack/InedoExtension/Properties/AssemblyInfo.cs
+++ YouTrack/InedoExtension/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using Inedo.Extensibility;
 [assembly: AssemblyTitle("YouTrack")]
 [assembly: AssemblyDescription("Contains an issue tracking source for JetBrains YouTrack, and operations for working with issues and fields.")]
 [assembly: AssemblyCompany("Inedo, LLC")]
-[assembly: AssemblyCopyright("Copyright © Inedo 2021")]
-[assembly: AssemblyVersion("2.4.0")]
-[assembly: AssemblyFileVersion("2.4.0")]
+[assembly: AssemblyCopyright("Copyright © Inedo 2024")]
+[assembly: AssemblyVersion("3.1.0")]
+[assembly: AssemblyFileVersion("3.1.0")]
 [assembly: CLSCompliant(false)]
 [assembly: ComVisible(false)]
 
